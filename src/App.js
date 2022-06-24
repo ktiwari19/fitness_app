9f3a281e16f493c8f9937d9e8de2699054671044
krishna <ktiwari19@gmail.com>
2022-06-24 10:21:09 +0530

    new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       tessting ci HJFHFJHFG
+         welcome
         </p>
         <a
           className="App-link"
